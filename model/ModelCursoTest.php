13899db38ca266d9adbca75b406b9e64385c1c3c
mamedioguilherme1 <mamedioguilherme@gmail.com>
2016-05-27 22:26:26 -0300

    The method "testConsultaCursos()"  has been created

--- model/ModelCursoTest.php
+++ model/ModelCursoTest.php
@@ -14,15 +14,23 @@ class ModelCursoTest extends PHPUnit_Framework_TestCase
  /**
   * Verifica se houve retorno na listagem dos cursos
   * @test esperado OK
-  */ 
+  */
  public function testListagemCursos()
  {
   $model = new ModelCurso();
   $resultado = $model->preparaListagem("cursos");
   $this->assertTrue(count($resultado) > 0 and !empty($resultado));
  }
-
-
+ /**
+  * Tenta buscar um curso inexistente
+  * @test esperado OK
+  */
+ public function testConsultaCursos()
+ {
+  $model = new ModelCurso();
+  $resultado = $model->preparaConsultaId(100, "cursos");
+  $this->assertFalse(count($resultado) > 0 and !empty($resultado));
+ }
 
 }
 
