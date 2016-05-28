a337a1d88c28a5b920fa57402042c8dee0c61663
Leonardo-Souza <leonardo-s@outlook.com>
2016-05-27 21:22:49 -0300

    Method test "testListagemCurso()" has been created.

---
+++ model/ModelCursoTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leonardo Vilarinho
+ * Date: 27/05/2016
+ * Time: 11:37
+ */
+
+require_once 'ModelCurso.aanc';
+
+class ModelCursoTest extends PHPUnit_Framework_TestCase
+{
+
+ /**
+  * Verifica se houve retorno na listagem dos cursos
+  * @test esperado OK
+  */
+ public function testListagemCursos()
+ {
+  $model = new ModelCurso();
+  $resultado = $model->preparaListagem("curso");
+  $this->assertTrue(count($resultado) > 0 and !empty($resultado));
+ }
+}
+
