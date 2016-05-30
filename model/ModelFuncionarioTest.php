8878181059524e715a20e7238eb1a6673cc22fae
mamedioguilherme1 <mamedioguilherme@gmail.com>
2016-05-30 16:45:43 -0300

    Method testConsultaFuncionario and testCriacaoFuncionarioError has been created, returned OK

--- model/ModelFuncionarioTest.php
+++ model/ModelFuncionarioTest.php
@@ -6,7 +6,7 @@
  * Time: 11:37
  */
 
-require_once 'ModelAluno.aanc';
+require_once 'ModelFuncionario.aanc';
 
 class ModelFuncionarioTest extends PHPUnit_Framework_TestCase
 {
@@ -17,10 +17,42 @@ class ModelFuncionarioTest extends PHPUnit_Framework_TestCase
      */
     public function testListagemFuncionarios()
     {
-        $model = new ModelAluno();
+        $model = new ModelFuncionario();
         $resultado = $model->preparaListagem("funcionarios");
         $this->assertTrue(count($resultado) > 0 and !empty($resultado));
     }
 
+    /**
+     * Tenta buscar um funcionario inexistente
+     * @test esperado OK
+     */
+    public function testConsultaFuncionario()
+    {
+        $model = new ModelFuncionario();
+        $resultado = $model->preparaConsultaId(100, "funcionarios");
+        $this->assertFalse(count($resultado) > 0 and !empty($resultado));
+    }
+
+    /**
+     * Tenta criar um funcionario com cpf invalido
+     * @test esperado OK
+     */
+    public function testCriacaoFuncionarioErro()
+    {
+        $this->setExpectedException("Exception", "CPF é inválido");
+        $model = new ModelFuncionario();
+        $dados = array
+        (
+            "tabela" => "funcionarios",
+            "consultar" => "cpf",
+            "nome" => "Girafales",
+            "cpf" => "12345678901",
+            "numero" => 3,
+            "email" => "girafales@outlook.com",
+            "telefone" => "000000000"
+        );
+        $resultado = $model->preparaCriacao($dados);
+        $this->assertFalse($resultado);
+    }
 
 }
